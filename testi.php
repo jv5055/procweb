55b21db41b518588e3b362cbffe3cc6290e9d098
jv5005 <111572051+jv5055@users.noreply.github.com>
2025-10-25 15:59:41 +0300

    remove unwanted key-value pairs from flattened array

--- testi.php
+++ testi.php
@@ -1,8 +1,9 @@
 <?php
 ini_set("allow_url_fopen", 1);
-$json = file_get_contents('https://www.unica.fi/menuapi/feed/json?costNumber=2000&language=fi');
+$json = file_get_contents('https://www.unica.fi/menuapi/feed/json?costNumber=198501&language=fi');
 $obj = json_decode($json, true);
 
+//litistetään toi kirottu nested array helpommaksi käsitellä
 function flatten($array, $prefix = '') {
     $result = [];
 
@@ -19,5 +20,15 @@ function flatten($array, $prefix = '') {
     return $result;
 }
 $flat = flatten($obj['MenusForDays']);
-print_r(array_values($flat));
+
+//poistetaan turhat key-value parit
+foreach ($flat as $key => $value) {
+    if (preg_match("/SortOrder|date/i", $key) == true) {
+        unset($flat[$key]);
+    }
+    if (str_starts_with($key, '1.')) {
+        unset($flat[$key]);
+    }
+}
+print_r($flat);
 ?>
