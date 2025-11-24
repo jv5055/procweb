e468ef884d98cb132f8b64b8348fac2e4348688b
Joel Viljanen <111572051+jv5055@users.noreply.github.com>
2025-11-24 15:31:06 +0200

    Delete menu.php
    delete unused file

--- menu.php
+++
@@ -1,9 +0,0 @@
-<?php
-ini_set("allow_url_fopen", 1);
-$json = file_get_contents('https://www.unica.fi/menuapi/feed/json?costNumber=1985&language=fi');
-$obj = json_decode($json, true);
-print_r($obj);
-
-
-
-?>
